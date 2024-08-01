985e02275453829f014cb6dcdad1ecab936f7c17
Hardy-Hao <ghost7021.hao@gmail.com>
2024-08-02 02:16:55 +0800

    feat:調整 vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import commonjs from 'vite-plugin-commonjs'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +13,32 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    commonjs(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true
+    })
   ],
+  base: '/byboGame-vue3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0'
+    }
   }
+  // build: {
+  //   target: 'es2020',
+  //   commonjsOptions: {
+  //     include: [/node_modules/, /@tonconnect\/sdk/]
+  //   }
+  // },
+  // define: {
+  //   global: 'globalThis'
+  // }
 })
